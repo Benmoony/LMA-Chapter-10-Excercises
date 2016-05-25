0a8735f3706d60f7772224e37a51cdbc3a2a8013
Miller, Benjamin A <benjaminmiller@cc1-230-403.student.cascadia.edu>
2016-05-25 15:45:21 -0700

    added a punch of stuff

--- Contacts/ContactsController.swift
+++ Contacts/ContactsController.swift
@@ -20,6 +20,30 @@ class ContactsController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    @IBOutlet weak var contact: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var cell: UITextField!
+    @IBOutlet weak var home: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zip: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var changedate: UIButton!
+    
+    @IBAction func sgmtSwitch(sender: AnyObject) {
+        //ask switch if its edit or view//
+        let segment = sender as! UISegmentedControl
+        
+        if (segment.titleForSegmentAtIndex(segment.selectedSegmentIndex) == "View"){
+            contact.enabled = false;
+        }
+        
+    }
+    
+    func onoff(passed: Bool){
+        
+    }
+    
 }
 
